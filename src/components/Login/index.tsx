ca4ec851a0ef890ae9da8bddeeb7bc85b14158dc
Caroline Ferraz <projetos.crrz@gmail.com>
2023-05-12 15:28:48 -0300

    Feat: Estrutura do documento HTML e estilização do componente Login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,8 +1,26 @@
-import './Login.css'
+import './Login.scss'
 
 const Login = () => {
     return (
-        <div></div>
+        <main>
+            <section>
+                <img src="./src/components/Login/assets/parrot-logo-blue.svg"></img>
+                <h1>Sysmap Parrot</h1>
+                <p>Faça seu login e comece a usar!</p>
+                <form>
+                    <div className="label-input">
+                        <label>Endereço de e-mail:</label>
+                        <input className="input-email" type="email" name="email" placeholder="  Digite seu e-mail"></input>
+                    </div>
+                    <div className="label-input">
+                        <label>Sua senha:</label>
+                        <input className="input-password" type="password" name="password" placeholder="  Digite sua senha"></input>
+                    </div>
+                    <button>Entrar</button>                  
+                </form>
+                <p>Não possui uma conta? <a href="">Crie uma agora!</a></p>
+            </section>
+        </main>
     )
 }
 
